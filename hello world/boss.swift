9fb52cd85dd8ffc72e6ed164482048e12ef1a6c8
Shirin Mujeeb <76579730+shirinmujeeb@users.noreply.github.com>
2021-01-06 16:16:13 +1000

    Add files via upload

---
+++ hello world/boss.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+struct boss:View {
+    var body:some View{
+        Text("shir")
+            .fontWeight(.bold)
+            .font(.title).frame(width: 1000, height: 1000)
+            .background(Color(.systemPink))
+            .foregroundColor(Color(.white))
+            
+}
+}
---
+++ hello world/hello_worldApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+//our expert
+@main
+struct hello_world: App {
+    var body:some Scene{
+        WindowGroup{
+            //type of scene
+            boss()
+            //no staircase
+        }
+    }
+}
